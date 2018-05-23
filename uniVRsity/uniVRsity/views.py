cb7bc032f15b938d9df0d9104464599cd37addfa
Eldor Bekpulatov <eldorbekpualtov@Eldors-MacBook-Pro.local>
2018-05-23 17:40:57 -0400

    Initial remote push

--- uniVRsity/uniVRsity/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def index(request):
-    # HttpResponse("Hello, world. You're at the polls index.")
-    return render(request, 'index.html')
