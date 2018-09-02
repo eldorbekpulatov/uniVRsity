8ecfb9bf7799b91bf7f960fe7887ad3513aa9562
Eldor Bekpulatov <eb654@cornell.edu>
2018-09-02 11:18:55 -0400

    settings update

--- uniVRsity/settings.py
+++ uniVRsity/settings.py
@@ -30,12 +30,12 @@ ALLOWED_HOSTS = ['univrsity.herokuapp.com', '127.0.0.1']
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 import dj_database_url
 db_from_env = dj_database_url.config()
